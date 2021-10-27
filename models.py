ff0d560ff8e01950b94fd55b74abf258b4b548b9
Eliya Habba <t-eliyahabba@microsoft.com>
2021-10-27 10:16:16 +0300

    Added simple model

--- models.py
+++ models.py
@@ -31,3 +31,19 @@ class Net(nn.Module):
         return x
 
 
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(9*26, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
