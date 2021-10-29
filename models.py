d73f4600d20c8eb9a7ae6f0c68ba1fbf3c1eb6ac
Eliya Habba <t-eliyahabba@microsoft.com>
2021-10-29 09:05:40 +0300

    deleted unnecessary code

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class NeuralNetwork(nn.Module):
         super(NeuralNetwork, self).__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(9*26, 128),
+            nn.Linear(9*20, 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
@@ -24,13 +24,30 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
+class NeuralNetworkBCEL(nn.Module):
+    def __init__(self):
+        super(NeuralNetworkBCEL, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(9*20, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return torch.sigmoid(logits)
+
 
 class BinaryClassification(nn.Module):
     def __init__(self):
         super(BinaryClassification, self).__init__()
         # Number of input features is 12.
         self.flatten = nn.Flatten()
-        self.layer_1 = nn.Linear(9*26, 64)
+        self.layer_1 = nn.Linear(9*20, 64)
         self.layer_2 = nn.Linear(64, 64)
         self.layer_out = nn.Linear(64, 2)
 
@@ -55,7 +72,7 @@ class BinaryClassificationOneLayer(nn.Module):
         super(BinaryClassificationOneLayer, self).__init__()
         # Number of input features is 12.
         self.flatten = nn.Flatten()
-        self.layer_1 = nn.Linear(9*26, 64)
+        self.layer_1 = nn.Linear(9*20, 64)
         self.layer_out = nn.Linear(64, 2)
 
         self.relu = nn.ReLU()
